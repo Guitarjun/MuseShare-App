4b09931bfdafe0b1b3ef699c694823ec967c7daf
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2021-11-22 21:16:01 -0800

    initial app setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './../App.css';
+
+function App() {
+  return (
+    <header className="main-page margin-200">
+        <div className="container">
+          <h1>MuseShare</h1>
+          <p className="motto">An open-source music collaboration platform!</p>
+        </div>
+    </header>
+    
+  );
+}
+
+export default App;
