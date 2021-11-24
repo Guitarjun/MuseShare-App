daf07a3d941d0b49c2e799709bf96618efebbfaa
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2021-11-24 15:38:36 -0800

    fixed project data

--- src/components/projectsPage/ProjectPage.js
+++ src/components/projectsPage/ProjectPage.js
@@ -17,7 +17,7 @@ export function ProjectPage(props) {
             <header className="background-brown">
                 <img className="mb-3" src={'../'+project.img} alt={project.name + " image"}/>
                 <h1>{project.name}</h1>
-                <h2>{project.artists}</h2>
+                <h2>{"by: " +project.artists}</h2>
                 <p>{project.genre}</p>
                 <a className="btn btn-dark mb-2" href="#" role="button"><span className="material-icons mb-3">play_arrow</span> 2:04  </a>
                 <a className="btn btn-primary" href="#" role="button">Download</a>
