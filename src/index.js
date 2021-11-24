8e1df1a4bcb8f1c00dc68feafdd3c4b03c644fd1
rheaguliani2 <rheagul2@uw.edu>
2021-11-24 21:03:26 +0530

    firebase changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase/app';
 
 
 ReactDOM.render(
