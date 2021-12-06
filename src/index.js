2c60f24dcc391cda226ce94db7d6e1e877e71ab1
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2021-12-06 15:44:19 -0800

    initialized firebase app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import "./App.css";
 import { initializeApp } from 'firebase/app';
+import firebase from "firebase/compat/app";
 
 // User and project data
 import projectsData from './data/projects.json';
 import userData from './data/users.json';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCpC441zDhyzmLRQst5scMgsRtRXz1CHZ0",
+  authDomain: "museshare-5e6ed.firebaseapp.com",
+  databaseURL: "https://museshare-5e6ed-default-rtdb.firebaseio.com",
+  projectId: "museshare-5e6ed",
+  storageBucket: "museshare-5e6ed.appspot.com",
+  messagingSenderId: "446196761869",
+  appId: "1:446196761869:web:719ba043119b5f10241c44",
+  measurementId: "G-WKJ5Y4S4F1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
